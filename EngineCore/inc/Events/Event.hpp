03619659129acd57a10c0e6688b6162a9e2f14a5
Napomosh <sashulik999@gmail.com>
2024-01-03 17:16:25 +0300

    Add external libs and declare Events class

---
+++ EngineCore/inc/Events/Event.hpp
@@ -0,0 +1,15 @@
+//
+// Created by sashu on 03.01.2024.
+//
+
+#ifndef SUNRISEENGINE_EVENT_HPP
+#define SUNRISEENGINE_EVENT_HPP
+
+namespace EngineCore {
+    class Event {
+
+    };
+}
+
+
+#endif //SUNRISEENGINE_EVENT_HPP
---
+++ EngineCore/src/Events/Event.cpp
@@ -0,0 +1,9 @@
+//
+// Created by sashu on 03.01.2024.
+//
+
+#include "../../inc/Events/Event.hpp"
+
+namespace EngineCore{
+
+}
